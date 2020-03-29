984dc2a181e550c8a991167645f6f3bb576763f4
Ismael <ismjaffri96@gmail.com>
2020-03-29 12:44:11 -0500

    Changed formatting of XML files with nav bar

--- app/src/main/java/com/example/pepintourapp/HelpFragment.java
+++ app/src/main/java/com/example/pepintourapp/HelpFragment.java
@@ -13,7 +13,7 @@ public class HelpFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_help, container, false);
+        return inflater.inflate(R.layout.fragment_help, container, false); //changed R.layout.fragment_help to
     }
 
 }
--- app/src/main/java/com/example/pepintourapp/HomeFragment.java
+++ app/src/main/java/com/example/pepintourapp/HomeFragment.java
@@ -14,7 +14,8 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false); //changed from fragment_home to activity_main.xml file, then back to fragment_home after
+        //changing the fragment_home to what was in activity_main.xml
     }
 
 }
