9df22a80ec02ecbe06702782bace463f1a2a525d
Andrew <ahuang33@wisc.edu>
2020-04-11 21:30:38 -0500

    more progress on destination activity page

--- app/src/main/java/com/example/pepintourapp/destinationActivity.java
+++ app/src/main/java/com/example/pepintourapp/destinationActivity.java
@@ -7,31 +7,59 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class destinationActivity extends AppCompatActivity {
 
-    destination currDest = new destination("temp");
+    ImageView image;
+    TextView txt;
+    destination currDest = new destination();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ImageView img= (ImageView) findViewById(R.id.imageView);
-        img.setImageResource(currDest.getFileName());
-        EditText myTextField = (EditText) findViewById(R.id.editText);
-        myTextField.setText(currDest.getDescription());
-        setContentView(R.layout.activity_destination);
+
+        setContentView(R.layout.activity_main);
+
+        //set image
+        image = (ImageView) findViewById(R.id.imageView);
+        image.setImageResource(currDest.getFileName());
+
+        //set text
+        txt = (TextView) findViewById(R.id.textView);
+        //txt.setText("this is trash");
+
+        String text = "N/A";
+        try{
+            InputStream inputStream = getAssets().open("file/t806.txt");
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+            text = new String(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        txt.setText(text);
+        //Toast.makeText(getApplicationContext(),text, Toast.LENGTH_LONG).show();
     }
 
-    public void backFunction(View view) {
-        Intent intent = new Intent(this, choose_destination.class);
-        startActivity(intent);
+    public void goToDestinations(View view) {
+        //Intent intent = new Intent(this, choose_destination.class);
+        //startActivity(intent);
+        Toast.makeText(this, "go to destination", Toast.LENGTH_SHORT).show();
     }
 
-    public void showFunction(View view) {
+    public void goToMap(View view) {
         //implement once map page is made
         /*
         * Intent intent = new Intent (this, Main2Activity.class);
             intent.putExtra("message", s);
             startActivity(intent);
          */
+        Toast.makeText(this, "go to map", Toast.LENGTH_SHORT).show();
     }
 }
